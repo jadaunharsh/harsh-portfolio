cce165fe62dae89bc735888b8961cbce0e93d70e
jadaunharsh <jadaunharsh92@gmail.com>
2023-10-29 01:20:49 +0530

    update docs files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,10 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/about', pathMatch: 'full' },
   { path: 'about', component: AboutComponent },
   { path: 'portfolio', component: PortfolioComponent },
   { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: 'about', pathMatch: 'full' },
 ];
 
 @NgModule({
